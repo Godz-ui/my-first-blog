22518a422dbc6d52e2be3542bfdd380f3de2bd59
Oda <you@example.com>
2025-10-08 14:21:14 +0500

    My Django app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
 def post_list(request):
